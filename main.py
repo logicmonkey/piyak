f5324a9ca3ad105729a011106c6252fb235fc50c
Piers <piers.barber@logicmonkey.co.uk>
2017-10-28 23:39:10 +0100

    kind how i want it. kinda working

---
+++ main.py
@@ -0,0 +1,100 @@
+from kivy.app import App
+
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.uix.label import Label
+from kivy.uix.scatter import Scatter
+from kivy.uix.behaviors import ToggleButtonBehavior, ButtonBehavior
+from kivy.uix.button import Button
+
+from kivy.properties import NumericProperty
+
+from kivy.clock import Clock
+
+from datetime import datetime
+
+class AppBox(BoxLayout):
+    pass
+
+class TelemetryBox(BoxLayout):
+
+    def __init__(self, **kwargs):
+        super(TelemetryBox, self).__init__(**kwargs)
+        Clock.schedule_interval(self.update, 1/60.0)
+
+        self.old_play_not_pause = 0
+        self.play_not_pause     = 0
+
+        # elapsed gets reset to a zero time delta
+        self.interval_start = datetime.now()
+        self.elapsed = self.interval_start - self.interval_start
+
+    def update(self, *args):
+
+        if self.ids.i_elapsed.ids.i_buttons.ids.i_reset.state == 'down':
+            self.old_play_not_pause               = 0
+            self.play_not_pause                   = 0
+            self.elapsed -= self.elapsed          # 0
+            self.ids.i_elapsed.ids.i_elapsed.text = '00:00:00'
+            self.ids.i_distspeed.ids.i_speed.text = '[b]0.0[/b] km/h'
+            self.ids.i_distspeed.ids.i_dist.text  = '[b]0[/b] m'
+            self.ids.i_gauge.angle                = 0.0
+
+        # read the play/pause button state
+        #
+        if self.ids.i_elapsed.ids.i_buttons.ids.i_playpause.state == 'down':
+            self.play_not_pause = 1
+        else:
+            self.play_not_pause = 0
+
+        # exiting pause, so take a snapshot of the time
+        #
+        if self.old_play_not_pause == 0 and self.play_not_pause == 1:
+            self.interval_start = datetime.now()
+
+        if self.play_not_pause == 1:
+
+            # this is how we enter pause from play
+            #
+            self.old_play_not_pause = self.play_not_pause
+
+            self.elapsed += datetime.now() - self.interval_start
+            hour, remr = divmod(self.elapsed.seconds, 60*60)
+            mins, secs = divmod(remr, 60)
+
+            self.ids.i_elapsed.ids.i_elapsed.text = "{:02d}:{:02d}:{:02d}".format(hour, mins, secs)
+
+            self.ids.i_distspeed.ids.i_speed.text = '[b]{0:.1f}[/b] km/h'.format(secs)
+            self.ids.i_distspeed.ids.i_dist.text  = '[b]{0:.0f}[/b] m'.format(secs)
+
+            self.ids.i_gauge.angle = -secs
+
+class GaugeBox(BoxLayout):
+    angle = NumericProperty(0)
+
+class DistSpeedBox(BoxLayout):
+    pass
+
+class ElapsedBox(BoxLayout):
+    pass
+
+class ButtonsBox(BoxLayout):
+    pass
+
+class PlayPauseButton(ToggleButtonBehavior, Image):
+    def __init__(self, **kwargs):
+        super(PlayPauseButton, self).__init__(**kwargs)
+        self.source = 'button_play.png'
+
+    def on_state(self, widget, value):
+        if value == 'down':
+            self.source = 'button_pause.png'
+        else:
+            self.source = 'button_play.png'
+
+class PiyakApp(App):
+    def build(self):
+        return AppBox()
+
+if __name__ == "__main__":
+    PiyakApp().run()
