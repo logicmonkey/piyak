1e9f05f589e7382a8a2709faa78d9b5118016a77
Piers Barber <piers.barber@logicmonkey.co.uk>
2018-01-19 23:58:56 +0000

    added rpm on right axis

--- forensic.py
+++ forensic.py
@@ -5,17 +5,17 @@ import csv
 import math
 import matplotlib.pyplot as plt
 
-# SI unit for moment of inertia is kg metres squared (not grammes)`
+# SI unit for moment of inertia is kg metres squared (not grammes)
 mass   = 4.360  # mass of Lawler flywheel in kilogrammes
 radius = 0.200  # radius of Lawler flywheel in metres
 
 def forensic(filename):
 
-    rpm=[]         # this doesn't really get used yet
-    period=[]      # neither does this - just the last thing pushed to it
+    period=[]      # not fully used - just the last thing pushed to it
 
     timestamp=[]
     energy=[]
+    rpm=[]
 
     with open(filename) as csvfile:
         alldata = csv.reader(csvfile, delimiter=',')
@@ -32,17 +32,39 @@ def forensic(filename):
                 # I  = 0.5*m*r^2 (half m radius squared)
                 # KE = 0.5*I*w^2 (half I omega squared)
                 energy.append(mass*(radius*math.pi/period[-1])**2)
-    return timestamp, energy
+    return timestamp, energy, rpm
 
+def share_axis(lyaxis, xdata, xlabel, lydata, lylabel, lycol, rydata, rylabel, rycol):
+    ryaxis = lyaxis.twinx()
+
+    lyaxis.plot(xdata, lydata, lycol)
+    lyaxis.set_xlabel(xlabel)
+    lyaxis.set_ylabel(lylabel)
+
+    ryaxis.plot(xdata, rydata, rycol)
+    ryaxis.set_ylabel(rylabel)
+    return lyaxis, ryaxis
+
+def colour_axis(axis, color):
+    for t in axis.get_yticklabels():
+        t.set_color(color)
+    return None
 
 if __name__ == '__main__' :
 
     filename = sys.argv[1]
 
-    timestamp, energy = forensic(filename)
+    timestamp, energy, rpm = forensic(filename)
 
-    plt.plot(timestamp, energy, 'r', marker='.', label='kinetic energy')
-    plt.ylabel('rotational kinetic energy (Joules)')
-    plt.xlabel('time (Seconds)')
+    llabel = 'Rotational Kinetic Energy (Joules)'
+    rlabel = 'RPM'
+    xlabel = 'Time (Seconds)'
+
+    fig, yaxis = plt.subplots()
+    ly, ry = share_axis(yaxis, timestamp, xlabel, energy, llabel, 'b-', rpm, rlabel, 'r.')
+
+    colour_axis(ly, 'b')
+    colour_axis(ry, 'r')
+
     plt.grid()
     plt.show()
