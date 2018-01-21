6e19a428f0862e118473d34b9eee75782dfe6ecd
Piers <piers.barber@logicmonkey.co.uk>
2018-01-21 01:17:59 +0000

    adding comment on the power calc - probbaly breaks dev branch :)

--- forensic.py
+++ forensic.py
@@ -9,6 +9,59 @@ import matplotlib.pyplot as plt
 mass   = 4.360  # mass of Lawler flywheel in kilogrammes
 radius = 0.200  # radius of Lawler flywheel in metres
 
+def calculate_power(energy):
+'''
+    1) look for an upward energy trend
+    2) the first measurement in an upward trend is a local minimum
+    3) look for a downward trend in energy
+    4) the first measurement in a downward trend is a local maximum
+    5) look for an upward energy trend
+    6) the first measurement in an upward trend is a local minimum
+
+    This is all the data required for analysis of a stroke.
+
+    These values are calculated from measured values of angular velocity:
+
+        E1     - starting rotational energy of the flywheel
+        E2     - local maximum energy at the end of a pull
+        E3     - energy that the flywheel decays to before the next stroke
+        tpull  - time difference between energies E2 and E1
+        tsetup - time difference between energies E3 and E2
+
+
+                               E2
+                               *. max
+                              /: `.
+                             / :   `.
+                            *  :     *.
+                           /   :       `. E4
+                          /    :<-tpull->`.
+                         *     :           *.    *
+                        /      :             `. /
+                       /       :<---tsetup---->* min
+                    \ /        :               E3
+                 min *<-tpull->:
+                    E1
+
+    E4 is the energy level dropped to from E2 over a period equal to tpull
+    as the flywheel spins down due to air resistance. It is calculated from
+    the spin down gradient
+
+    Ein is the total energy put into the flywheel plus the energy lost to
+    air resistance of the fan. Ein = E2-E1+E4
+
+                       (E2-E3)
+    Ein = E2-E1 + tpull--------
+                        tsetup
+
+    Pin is the power of the stroke (energy over total time)
+
+              Ein
+    Pin = ------------
+          tpull+tsetup
+
+'''
+
 def forensic(filename):
 
     period=[]      # not fully used - just the last thing pushed to it
@@ -32,7 +85,7 @@ def forensic(filename):
                 # I  = 0.5*m*r^2 (half m radius squared)
                 # KE = 0.5*I*w^2 (half I omega squared)
                 energy.append(mass*(radius*math.pi/period[-1])**2)
-    return timestamp, energy, rpm
+    return timestamp, energy, rpm, power
 
 def share_axis(lyaxis, xdata, xlabel, lydata, lylabel, lycol, rydata, rylabel, rycol):
     ryaxis = lyaxis.twinx()
