6d0b69ec9dcff6ea029fb3be72de0141c3f120f8
Piers <piers.barber@logicmonkey.co.uk>
2018-01-21 15:28:40 +0000

    added everything as subplots with a shared x axis - much easier

--- forensic.py
+++ forensic.py
@@ -9,38 +9,42 @@ import matplotlib.pyplot as plt
 mass   = 4.360  # mass of Lawler flywheel in kilogrammes
 radius = 0.200  # radius of Lawler flywheel in metres
 
-def calculate_power(energy):
-'''
+def calculate_power(energy, timestamp):
+    '''
+    Identify individual Strokes
+
     1) look for an upward energy trend
-    2) the first measurement in an upward trend is a local minimum
+       the first measurement in an upward trend is a local minimum
     3) look for a downward trend in energy
-    4) the first measurement in a downward trend is a local maximum
+       the first measurement in a downward trend is a local maximum
     5) look for an upward energy trend
-    6) the first measurement in an upward trend is a local minimum
+       the first measurement in an upward trend is a local minimum
 
     This is all the data required for analysis of a stroke.
+
+    Calculate Rate of Energy Input
 
     These values are calculated from measured values of angular velocity:
 
         E1     - starting rotational energy of the flywheel
         E2     - local maximum energy at the end of a pull
         E3     - energy that the flywheel decays to before the next stroke
-        tpull  - time difference between energies E2 and E1
-        tsetup - time difference between energies E3 and E2
+        tpull  - time difference between energies E2 and E1 (t2-t1)
+        tsetup - time difference between energies E3 and E2 (t3-t2)
 
 
                                E2
-                               *. max
+                               o. max
                               /: `.
                              / :   `.
-                            *  :     *.
+                            o  :     o.
                            /   :       `. E4
                           /    :<-tpull->`.
-                         *     :           *.    *
+                         o     :           o.    o
                         /      :             `. /
-                       /       :<---tsetup---->* min
+                       /       :<---tsetup---->o min
                     \ /        :               E3
-                 min *<-tpull->:
+                 min o<-tpull->:
                     E1
 
     E4 is the energy level dropped to from E2 over a period equal to tpull
@@ -51,16 +55,53 @@ def calculate_power(energy):
     air resistance of the fan. Ein = E2-E1+E4
 
                        (E2-E3)
-    Ein = E2-E1 + tpull--------
+    Ein = E2-E1 + tpull--------          (1)
                         tsetup
 
     Pin is the power of the stroke (energy over total time)
 
               Ein
-    Pin = ------------
+    Pin = ------------                   (2)
           tpull+tsetup
 
-'''
+    '''
+
+    power=[]
+
+    looking_for_e1 = True     # start here in hunt for first local minimum
+    looking_for_e2 = False    # then alternate between this state and the next
+    looking_for_e3 = False
+
+    stroke_power = 0
+
+    for i, v in enumerate(energy[:-1]): # loop over all i, i+1 pairs
+
+        power.append(stroke_power) # redundancy keeps data sets the same size
+
+        if looking_for_e1 and v < energy[i+1]:
+            local_min = (v, timestamp[i])
+            looking_for_e1 = False
+            looking_for_e2 = True
+
+        elif looking_for_e2 and v > energy[i+1]:
+            local_max = (v, timestamp[i])
+            looking_for_e2 = False
+            looking_for_e3 = True
+
+        elif looking_for_e3 and v < energy[i+1]:
+            e1, t1 = local_min
+            e2, t2 = local_max
+            e3, t3 = v, timestamp[i]
+
+            stroke_power = (e2-e1+(t2-t1)*(e2-e3)/(t3-t2))/(t3-t1) # eqs.(1,2)
+
+            local_min = (e3, t3) # e3 is the next e1
+            looking_for_e2 = True
+            looking_for_e3 = False
+
+    # push a final value as there is one less interval than energy data points
+    power.append(stroke_power)
+    return power
 
 def forensic(filename):
 
@@ -85,39 +126,36 @@ def forensic(filename):
                 # I  = 0.5*m*r^2 (half m radius squared)
                 # KE = 0.5*I*w^2 (half I omega squared)
                 energy.append(mass*(radius*math.pi/period[-1])**2)
+
+    power = calculate_power(energy, timestamp)
+
     return timestamp, energy, rpm, power
-
-def share_axis(lyaxis, xdata, xlabel, lydata, lylabel, lycol, rydata, rylabel, rycol):
-    ryaxis = lyaxis.twinx()
-
-    lyaxis.plot(xdata, lydata, lycol)
-    lyaxis.set_xlabel(xlabel)
-    lyaxis.set_ylabel(lylabel)
-
-    ryaxis.plot(xdata, rydata, rycol)
-    ryaxis.set_ylabel(rylabel)
-    return lyaxis, ryaxis
-
-def colour_axis(axis, color):
-    for t in axis.get_yticklabels():
-        t.set_color(color)
-    return None
 
 if __name__ == '__main__' :
 
     filename = sys.argv[1]
 
-    timestamp, energy, rpm = forensic(filename)
+    timestamp, energy, rpm, power = forensic(filename)
 
-    llabel = 'RPM'
-    rlabel = 'Rotational Kinetic Energy (Joules)'
-    xlabel = 'Time (Seconds)'
+    rlabel = 'Revolutions\n(per minute)'
+    elabel = 'Rotational Energy\n(joules)'
+    plabel = 'Power\n(watts)'
+    xlabel = 'Time (seconds)'
 
-    fig, yaxis = plt.subplots()
-    ly, ry = share_axis(yaxis, timestamp, xlabel, rpm, llabel, 'b-', energy, rlabel, 'r.')
+    fig, (raxes, eaxes, paxes) = plt.subplots(3, sharex=True)
 
-    colour_axis(ly, 'r')
-    colour_axis(ry, 'b')
+    raxes.plot(timestamp, rpm, 'g-')
+    raxes.grid(b=True)
+    raxes.set_ylabel(rlabel)
 
-    plt.grid()
+    eaxes.plot(timestamp, energy, 'b-')
+    eaxes.grid(b=True)
+    eaxes.set_ylabel(elabel)
+
+    paxes.plot(timestamp, power, 'r-')
+    paxes.grid(b=True)
+    paxes.set_ylabel(plabel)
+
+    paxes.set_xlabel(xlabel)
+
     plt.show()
