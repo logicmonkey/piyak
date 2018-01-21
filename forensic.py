c1fa09504d6d99395152b68d7feae96cfc24a63a
Piers <piers.barber@logicmonkey.co.uk>
2018-01-21 19:57:20 +0000

    added copyright notice

--- forensic.py
+++ forensic.py
@@ -1,5 +1,35 @@
 #!/usr/bin/env python
 
+'''
+Forensic - data analysis part of the Piyak kayak simulator ergo software for
+           use on Lawler ergos. This software reads forensic_yyyymmdd.csv
+           files and calculates athlete power output. The calculation is
+           given below and relies upon flywheel mass (to weigh it, you will
+           have to dismantle your machine - a bit :)
+
+Copyright (c) 2017-18 Piers Barber
+
+This is free software released under the terms of the MIT licence
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+
 import sys
 import csv
 import math
