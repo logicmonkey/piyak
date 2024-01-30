50183266a4101a7334ad64fc7d6440c906230b37
Piers Barber <piers.barber@logicmonkey.co.uk>
2024-01-30 08:53:00 +0000

    Simplified the file read
    - read single column of integers
    - no need for csv

--- report.py
+++ report.py
@@ -46,7 +46,6 @@ THE SOFTWARE.
 '''
 
 import sys
-import csv
 import math
 import re
 import matplotlib.pyplot as plt
@@ -210,30 +209,29 @@ def calculate_power(energy, timestamp):
 def forensic(filename):
 
     # SI unit for moment of inertia is kg metres squared (not grammes)
-    mass   = 4.360  # mass of Lawler flywheel in kilogrammes
-    radius = 0.200  # radius of Lawler flywheel in metres
-
-    period = []     # not fully used - just the last thing pushed to it
+    MASS   = 4.360  # mass of Lawler flywheel in kilogrammes
+    RADIUS = 0.200  # radius of Lawler flywheel in metres
+    PI     = math.pi
 
     timestamp = []
     energy = []
     rpm = []
 
-    with open(filename) as csvfile:
-        alldata = csv.reader(csvfile, delimiter=',')
-        for row in alldata:
-            if row[2] != 'None' and row[2] != '0':
-                h, m, s = row[0].split(':')
-                timestamp.append(int(h)*3600 + int(m)*60 + float(s))
-                # the millisecond rotation periods into seconds
-                period.append(int(row[2])*1.0e-6)
-                # current rotation period to frequency in rpm
-                rpm.append(60.0/period[-1])
+    for usec in open(filename):
+        if usec != 'None' and usec != '0':
+            # the microsecond rotation periods into seconds
+            period = int(usec)*1.0e-6
+            if len(timestamp)==0:
+                timestamp.append(period)
+            else:
+                timestamp.append(timestamp[-1]+period)
+            # current rotation period to frequency in rpm
+            rpm.append(60.0/period)
 
-                # w  = 2*pi/period (angular velocity omega = 2 pi radians * revolutions/second)
-                # I  = 0.5*m*r^2 (half m radius squared)
-                # KE = 0.5*I*w^2 (half I omega squared)
-                energy.append(mass*(radius*math.pi/period[-1])**2)
+            # w  = 2*pi/period (angular velocity omega = 2 pi radians * revolutions/second)
+            # I  = 0.5*m*r^2 (half m radius squared)
+            # KE = 0.5*I*w^2 (half I omega squared)
+            energy.append(MASS*(RADIUS*PI/period)**2)
 
     fpower, fstroke = calculate_power(energy, timestamp)
 
